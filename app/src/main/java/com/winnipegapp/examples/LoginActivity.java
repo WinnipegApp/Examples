f4242143543dc1c1f0663a76b2d81a0b0feb5e36
Ninja Mo <fagundesm86@gmail.com>
2016-07-21 12:49:37 -0500

    Merge**

--- app/src/main/java/com/winnipegapp/examples/LoginActivity.java
+++ app/src/main/java/com/winnipegapp/examples/LoginActivity.java
@@ -26,7 +26,7 @@ public class LoginActivity extends AppCompatActivity
         {
             public void onClick(View v)
             {
-                startActivity(new Intent(LoginActivity.this, LoginActivity));
+                startActivity(new Intent(LoginActivity.this, LoginActivity.class));
             }
         });
 
